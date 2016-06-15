413d463720930e88d30ea57de35d9ec0791e1717
irati rasines garcia <irati rasines garcia>
2016-06-15 19:47:15 +0200

    base file for warping added. the code has to be written

---
+++ src/2.- Warp the Curves/runWarpCurvesByAction.m
@@ -0,0 +1,17 @@
+
+
+%% Code for creating warped action files.
+
+% the files have to be warped per action, this is, all the examples of the
+% same actions are going to be warped 
+
+%% Load the data
+
+
+%% Separate in cells per action
+
+
+%% Let's warp!!!
+
+
+%% Store the curves
